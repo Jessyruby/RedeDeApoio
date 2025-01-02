fa0969efcf25ec17ae28a52385d917e0e867c665
Jessica Cardoso <145391050+Jessyruby@users.noreply.github.com>
2025-01-02 17:02:02 -0300

    update

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+
+    const email = document.getElementById('email').value;
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+    if (!emailPattern.test(email)) {
+        alert('Por favor, insira um e-mail válido.');
+        event.preventDefault(); 
+    } else {
+        alert('Email enviado!');
+    }
+});
+
